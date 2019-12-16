011a369aea43580323fa1e2e153c6e47c3559a21
Douglas Cahl <dougcahl@gmail.com>
2019-12-16 12:47:13 -0500

    paths

--- add_paths.m
+++ add_paths.m
@@ -5,9 +5,6 @@ addpath(genpath(hfrprogs_path))
 m_map_path = '..\m_map';
 addpath(m_map_path);
 
-matWERA_path = '..\matWERA';
-addpath(matWERA_path);
-
 lera_DPd_path = '..\lera_DPd';
 addpath(genpath(lera_DPd_path));
 
